b4e67e1e44fadf0993bba46eada2a286b4dc3c29
Girjesh Rajoria <32087575+grajoria@users.noreply.github.com>
2023-11-20 19:59:04 +0000

    Update RangeQueryPerformanceTest.java

--- src/RangeQueryPerformanceTest.java
+++ src/RangeQueryPerformanceTest.java
@@ -14,27 +14,36 @@ public class RangeQueryPerformanceTest {
     @Before
     public void setUp() {
         RangeContainerFactory rf = new YourRangeContainerFactory();
-        rc = rf.createContainer(generateRandomDataset(DATASET_SIZE));
+        rc = rf.createContainer(generateRandomDataset());
     }
 
     @Test
     public void testPerformance() {
         long startTime = System.nanoTime();
-
-        Ids ids = rc.findIdsInRange(1000, 5000, true, false);
-
+        Ids ids = rc.findIdsInRange(-2000000000, 30000000, true, false);
         long endTime = System.nanoTime();
+        System.out.println("findIdsInRange() execution time: " + (endTime - startTime) / 1000 + " micro seconds");
 
-        System.out.println("Query execution time: " + (endTime - startTime) / 1000 + " micro seconds");
+
+        int count = 0;
+        startTime = System.nanoTime();
+        while(ids.nextId() != Ids.END_OF_IDS) {
+            count++;
+        }
+        endTime = System.nanoTime();
+        if (count != 0) {
+            System.out.println("average nextId() execution time: " + (endTime - startTime) / count + "nano seconds");
+        }
     }
 
-    private long[] generateRandomDataset(int size) {
+    private long[] generateRandomDataset() {
+
         Random random = new Random();
-        long[] data = new long[size];
-        for (int i = 0; i < size; i++) {
-            data[i] = random.nextLong();
+        long[] data = new long[DATASET_SIZE];
+        for (int i = 0; i < DATASET_SIZE; i++) {
+            data[i] = random.nextInt();
+            // System.out.println(data[i] + ", ");
         }
         return data;
     }
 }
-
